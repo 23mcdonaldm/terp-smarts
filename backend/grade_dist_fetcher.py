c60feefcdd88f914abfe71d9981b24f6fc2cc1bb
Milesm <miles1301@icloud.com>
2025-05-17 22:43:54 -0400

    fetches courses data from planet-terp api

--- backend/grade_dist_fetcher.py
+++ backend/grade_dist_fetcher.py
@@ -1,53 +1,72 @@
 import requests
 import time
 import pandas as pd
-import json
 
-# around 13,400 professors, 100 records max per api call, ~134 total
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+# around x courses, 100 records max per api call
 offset = 0
 courses_data = []
 
-for i in range(0, 135):
-  url = f'https://planetterp.com/api/v1/professors?type=professor&reviews=true&offset={offset}'
+while True:
+    url = f'https://planetterp.com/api/v1/courses?reviews=true&offset={offset}'
 
-  try:
+    try:
         r = requests.get(url, timeout=120)
-  except requests.exceptions.Timeout:
+    except requests.exceptions.Timeout:
         print(f"Timeout occurred at offset {offset}. Retrying...")
         continue
 
-  if r.status_code in [500,524]:
+    if r.status_code in [500,524]:
         print(f"Received {r.status_code} timeout at offset {offset}. Retrying after delay...")
         time.sleep(5)
         continue
 
-
-  if r.status_code != 200:
+    if r.status_code != 200:
         print(f"Error: Received status code {r.status_code}")
         break
 
-
-
-  try:
-    data = r.json();
-  except Exception as e:
+    try:
+        data = r.json();
+    except Exception as e:
         print(f"Failed to parse JSON at offset {offset}: {e}")
         break
 
-  if not data:
-    print("No more data. Exiting loop.")
-    break
+    if not data:
+        print("No more data. Exiting loop.")
+        break
 
-  print(f"Adding professors {offset}-{offset+100}")
-  print(data)
-  professors_data.extend(data)
-  offset += 100
+    print(f"Adding courses {offset}-{offset+100}")
+    print(data)
+    courses_data.extend(data)
+    offset += 100
 
-  if (offset > 15000):
-    print("Offset too high. Breaking manually.")
-    break
+    if (offset > 15000):
+        print("Offset too high. Breaking manually.")
+        break
 
 
 
-print(f"Total professors fetched: {len(professors_data)}")
-print(professors_data)
+print(f"Total courses fetched: {len(courses_data)}")
+
+## all data in courses_data, now need to get the grade distribution for each course
+
+
+
+
+
+
+## putting in db
+
+load_dotenv()
+db_url = os.getenv("DATABASE_URL")
+
+# conn = psycopg2.connect(db_url)
+# cur = conn.cursor()
+
+# cur.execute("SELECT 1;")
+# print(cur.fetchone())
+
+# conn.close()
